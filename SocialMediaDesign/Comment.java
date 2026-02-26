b492ea5753745300aeb41e02d6412cc7574da492
amirali <amiralizarei2005@gmail.com>
2026-02-26 14:53:37 +0330

    Create Comment.java

---
+++ SocialMediaDesign/Comment.java
@@ -0,0 +1,29 @@
+public class Comment
+{
+    private String author;
+    private String text;
+
+    public Comment(String author, String text)
+    {
+        this.author = author;
+        this.text = text;
+    }
+
+    public String getAuthor()
+    {
+        return author;
+    }
+
+    public String getText()
+    {
+        return text;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
